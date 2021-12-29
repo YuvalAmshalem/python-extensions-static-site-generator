a9a671eb3cfe7e33ae6d7c2e43a92c59f2ca5279
YuvalAmshalem <yuval3107@gmail.com>
2021-12-29 14:09:51 +0200

    'python-extensions-static-site-generator'

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -48,9 +48,8 @@ class MarkdownParser(Parser):
         html = markdown(content.body)
         filtered=hooks.filter("generate_menu",html,self.base_ext)
         self.write(path, dest, filtered)
-        sys.stdout.write(
-            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
-        )
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+        hooks.event("written")
 
 
 class ReStructuredTextParser(Parser):
@@ -61,6 +60,5 @@ class ReStructuredTextParser(Parser):
         html = publish_parts(content.body, writer_name="html5")
         filtered=hooks.filter("generate_menu",html["html_body"],self.base_ext)
         self.write(path, dest, filtered)
-        sys.stdout.write(
-            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
-        )
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+        hooks.event("written")
